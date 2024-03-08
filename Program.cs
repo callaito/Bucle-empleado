14be8ed508e51529850eefffede7a03c2295e1d2
callaito <radelinrsm029@gmail.com>
2024-03-07 20:40:11 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Determinar la cantidad de empleados
+
+
+//Declarar siempre primero
+
+double sueldo, total, promedio;
+
+//Inicializar siempre antes del bucle
+
+Console.WriteLine("Cantidad de empleados: ");
+int cantidad = int.Parse(Console.ReadLine());
+total = 0;
+
+for (int i = 1; i <= cantidad; i++)
+{
+
+    Console.WriteLine("Sueldo del empleado " + i);
+    sueldo = Convert.ToDouble(Console.ReadLine());
+    total += sueldo;
+}
+
+//Calcular el promedio
+promedio = total / cantidad;
+
+Console.WriteLine("El sueldo promedio de um empleado es: " + promedio);
